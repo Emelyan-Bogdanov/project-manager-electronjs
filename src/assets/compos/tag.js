1ed08d86de1e35b6fe78a6da53db4175fe32a5e6
Емельян Богданов (idwahman) <luciusartiuscastus68@gmail.com>
2026-05-11 16:27:11 +0100

    added Vue js  , to make tag component

---
+++ src/assets/compos/tag.js
@@ -0,0 +1,21 @@
+// create the component
+Vue.component("tag-compo", {
+  name: "tag-compo",
+  props: ["label","count"], // html tags like : <post-compo author="ibrahim"></post-compo>
+  data: function () {
+    // unlike app , the compo's data has to be a function
+    return {
+      checked:false
+    };
+  },
+  methods: {
+    check: function () {
+      this.checked = !this.checked;
+    },
+  },
+  template: `<div class="tag">
+                <input class="form-check-input" type="checkbox" {{checked?'checked':''}}/>
+                <span class="tag-name">{{label}}</span>
+                <span class="count">{{count}}</span>
+          </div>`,
+});
