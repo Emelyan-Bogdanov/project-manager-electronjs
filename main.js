94e73090004f8812a628128f7af615dd8e2abff6
Емельян Богданов (idwahman) <luciusartiuscastus68@gmail.com>
2026-05-12 18:02:54 +0100

    from users to ui
    [DONE] : get users from flask api , to render them as a vuejs components
    [TODO] : do the same thing for tasks + workspaces

--- main.js
+++ main.js
@@ -8,26 +8,37 @@ let win;
 // main window
 function MainApp() {
   win = new BrowserWindow({
-    width: 1024,
-    height: 500,
-    minWidth: 850,
-    minHeight: 450,
+    width: 1400,
+    height: 730,
+    minWidth: 1400,
+    minHeight: 730,
     // movable: false, // position fixe
     icon: "../assets/imgs/icon.png",
     title: "Gestionnaire de projets",
     center: true,
 
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: true, 
+      enableRemoteModule: false,
+      preload: path.join(__dirname, "preload.js"), // use a preload script
+    },
+
     // frame:false // remove top bar buttons [minimize,fullscreen,close]
   });
 
   // win.loadFile("./window.js")
-  win.loadFile("src/templates/enhanced-index.html");
+  win.loadFile("src/templates/index.html");
 
   ipcMain.on("open-profile", () => {
     win.loadFile("src/templates/profile.html");
   });
 
-   win.on('closed', () => {
+  ipcMain.on("all-users", () => {
+    win.loadFile("src/templates/allUsers.html");
+  });
+
+  win.on("closed", () => {
     win = null;
   });
 }
